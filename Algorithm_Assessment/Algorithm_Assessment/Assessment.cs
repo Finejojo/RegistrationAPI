cf6bee2a97ce661031f87bc3115d7ef08b7ae6b8
finejojo <godfreyjo2015@gmail.com>
2024-03-05 19:10:32 +0100

    Alogorithm Assessment

---
+++ Algorithm_Assessment/Algorithm_Assessment/Assessment.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Assessment
+{
+    public static class Assessment
+    {
+        public static string FindLongestCommonPrefix(List<string> input)
+        {
+            if(input == null || input.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            //Taking the first string as reference
+            var stringOfReference = input[0];
+
+
+            for (int i = 0; i < stringOfReference.Length; i++)
+            {
+                char currentChar = stringOfReference[i];
+
+                
+                for (int j = 1; j < input.Count; j++)
+                {
+                    // If the index exceeds the length of the current string or the character is different, return the prefix
+                    if (i >= input[j].Length || input[j][i] != currentChar)
+                        return stringOfReference.Substring(0, i);
+                }
+            }
+
+            return stringOfReference;
+        }
+    }
+}
---
+++ Algorithm_Assessment/Algorithm_Assessment/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Algorithm_Assessment;
+
+var inputStrings = new List<string> { "fliower", "fliow", "flight", "flight" };
+
+var longestCommonPrefix = Assessment.FindLongestCommonPrefix(inputStrings);
+
+
+Console.WriteLine($"Longest Common Prefix: {longestCommonPrefix}");
